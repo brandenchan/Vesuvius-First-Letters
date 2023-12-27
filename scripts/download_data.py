aa072dfb15f7ae1aa028c95b848807eacda74fdb
brandenchan <brandenchan@icloud.com>
2023-12-27 21:00:12 +0100

    Allow for download args

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,8 +1,12 @@
 from vesuvius.downloader import download_segment_files
 from tap import tap
 
+class Arguments(tap):
+    segment_id: str
+
+args = Arguments().parse_args().as_dict()
 
 download_segment_files(
-    segment_id="20230504093154",
+    segment_id=args["segment_id"],
     save_dir="data"
 )
